ca112fd891c1d035d4555cbfb277d82bf5f55caa
nazriexplorer <ahmadrezamirzaei38@gmail.com>
2021-06-24 17:35:28 +0430

    aaa

--- MEM/sram.sv
+++ MEM/sram.sv
@@ -7,16 +7,19 @@ module sram (
     inout [31:0] data_SRAM
 );
 
-    reg signed [31:0] memory [0:511];
+    parameter RAM_WIDTH = 32;                  // Specify RAM data width
+    parameter RAM_DEPTH = 512;                  // Specify RAM depth (number of entries)
+
+    reg signed [RAM_WIDTH-1:0] memory [0:RAM_DEPTH-1];
     integer i;
     integer fd;
 
-    assign #30 data_SRAM = WE_N_SRAM ? memory[address_SRAM] : {32{1'bz}};
+    assign #30 data_SRAM = WE_N_SRAM ? memory[address_SRAM] : {RAM_WIDTH{1'bz}};
 
     initial begin
         fd = $fopen("./build/memory.txt", "w");
         $fdisplay(fd, "MEMORY--------------------------------------------------------------------");
-        for (i = 0; i<512; i=i+1) begin
+        for (i = 0; i<RAM_DEPTH; i=i+1) begin
             memory[i] = 0;
             $fdisplay(fd, "%d: %d", 4*i+1024, memory[i]);
         end
@@ -27,7 +30,7 @@ module sram (
         if (rst) begin
             fd = $fopen("./build/memory.txt", "w");
             $fdisplay(fd, "MEMORY--------------------------------------------------------------------");
-            for (i = 0; i<512; i=i+1) begin
+            for (i = 0; i<RAM_DEPTH; i=i+1) begin
                 memory[i] = 0;
                 $fdisplay(fd, "%d: %d", 4*i+1024, memory[i]);
             end
@@ -37,7 +40,7 @@ module sram (
             memory[address_SRAM] = data_SRAM;
             fd = $fopen("./build/memory.txt", "w");
             $fdisplay(fd, "MEMORY--------------------------------------------------------------------");
-            for (i = 0; i<512; i=i+1)
+            for (i = 0; i<RAM_DEPTH; i=i+1)
                 $fdisplay(fd, "%d: %d", 4*i+1024, memory[i]);
             $fclose(fd);
         end
